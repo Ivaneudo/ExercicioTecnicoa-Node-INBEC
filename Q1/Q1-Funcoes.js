d05c81d2616a6ae58e6e8ec0e1c3ff32091d61ab
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-06 12:21:25 -0300

    fix: Concertando o uso estável do sistema

--- Q1/Q1-Funcoes.js
+++ Q1/Q1-Funcoes.js
@@ -1,7 +1,7 @@
 function VerificaPorcentagem(cpu, ram) {
   if (cpu >= 80 || ram >= 80) {
     return `ALERTA: Sistema Sobrecarregado.`;
-  } else if (cpu <= 50 && ram <= 50) {
+  } else if (cpu < 50 && ram < 50) {
     return `Sistema Estável.`;
   } else {
     return `ATENÇÃO: Uso Moderado`;
